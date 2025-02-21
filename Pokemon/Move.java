e92d609b42f97e94c47de9c05a43ca4e3009e748
phosphenia <oliver.n.thy@gmail.com>
2025-02-21 11:50:22 +0100

    Lidt mere stuff samme som sidst

--- Pokemon/Move.java
+++ Pokemon/Move.java
@@ -15,6 +15,7 @@ public class Move {
     private int StatusChance;
     private int critChance;
     private final double critMultiplier = 2.0;
+    private int Damage = 0;
 
     public Move(String Name, int Pwr, int Acc, boolean isSpcl, boolean hasPrio, boolean isStab, String type, String typeReq, boolean inflictsStatus, String StatusType, int StatusChance, int critChance) {
         this.Name = Name;
@@ -31,10 +32,13 @@ public class Move {
 
     public void performMove(Pokemon user, Pokemon victim) {
         // Mangler CritBoost metode logik (skal implementeres i Move som setter Boost parametre i Pokemon)
-        int Damage = (((((2 * user.getLevel() * (this.critChance <= 6 * user.getCritBoost() ? critMultiplier : 1)) / 5 + 2) * this.Power * 
-        (this.isSpecial == true ? user.getSpA() / victim.getSpDef() : user.getAtt() / victim.getSpDef())) / 50) + 2) * (this.isStab == true ? 1.5 : 1)
-        * 
+        if (this.Power > 0) {
+            int Damage = (((((2 * user.getLevel() * (this.critChance <= 6 * user.getCritBoost() ? critMultiplier : 1)) / 5 + 2) * this.Power * 
+            (this.isSpecial == true ? user.getSpA() / victim.getSpDef() : user.getAtt() / victim.getSpDef()))+100) / 50) * (this.isStab == true ? 1.5 : 1)
+            * //type chart missing (skal implementeres som en class med separate metoder for hver type, for immune, effektiv, normal og ueffektiv)
+            // if (Damage <1 && victim.getType1MODIFIER * victim.getType2MODIFIER > 0)
 
+        }
 
     }
 

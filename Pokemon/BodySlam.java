243159e26a88b32ada37ad4467f25a45b38c783e
BenjaminDanLol <benjaminlolschou@gmail.com>
2025-02-25 20:37:45 +0000

    Added Moves no effects
    Var bare nødt til at gøre det

---
+++ Pokemon/BodySlam.java
@@ -0,0 +1,8 @@
+public class BodySlam extends Move {
+    public BodySlam() {
+        super("Body Slam", 85, 100, false, false, 
+        "Normal", "Electric", 30, 
+        10, "paralysis"); 
+        }    
+    }
+
---
+++ Pokemon/SwordsDance.java
@@ -0,0 +1,7 @@
+public class SwordsDance extends Move {
+    public SwordsDance(){
+    super("Swords Dance", 0, 100, false, false, 
+    "Normal", "None", 0, 
+    10, "None"); 
+    }
+}
